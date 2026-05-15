d9d00834bbd688d6c1650a7185a9cc7fc29ccc29
Samiul Hak <samiulhak785@gmail.com>
2026-05-15 21:08:11 +0530

    Uppercase.java

---
+++ UpperCase.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String upper = str.toUpperCase();
+
+        System.out.println("Uppercase = " + upper);
+
+        sc.close();
+    }
+}
