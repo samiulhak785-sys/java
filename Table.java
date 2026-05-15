dcbcb07909fcc8fb5972358299bd2fdef46bc63d
Samiul Hak <samiulhak785@gmail.com>
2026-05-15 18:50:01 +0530

    Table.java

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long num = sc.nextLong();
+
+        for (long i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        sc.close();
+    }
+}
