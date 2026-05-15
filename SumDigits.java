73d00afccc6cd51e662bad3fe6d0ea8dc40dc464
Samiul Hak <samiulhak785@gmail.com>
2026-05-15 18:35:07 +0530

    sumdigit.java

---
+++ SumDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long num = sc.nextLong();
+
+        long sum = 0;
+
+        while (num != 0) {
+            long digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
+    }
+}
